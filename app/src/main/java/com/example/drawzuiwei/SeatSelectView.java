22246d57a20544a1ddaccef3aaa42bd1b429c140
chendexiang <937682896@qq.com>
2020-09-18 10:38:30 +0800

    修改Bug。

--- app/src/main/java/com/example/drawzuiwei/SeatSelectView.java
+++ app/src/main/java/com/example/drawzuiwei/SeatSelectView.java
@@ -375,6 +375,7 @@ public class SeatSelectView extends SurfaceView implements SurfaceHolder.Callbac
         // 获得容器的宽高
         containerWidth = getWidth();
         containerHeight = getHeight();
+        widthHeight = containerWidth / 11;
 
         // 获取xpoint和ypoint的最大值和最小值
         MaxMin maxMinPoint = getMaxMinPoint();
